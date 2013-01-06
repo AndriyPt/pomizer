5614385728548d9a6c79613482c142f2715b333e
AndriyPt@gmail.com <AndriyPt@gmail.com@96db4dfc-c75a-a41e-586d-6ff218abbca7>
2013-01-06 11:47:06 +0000

    First version. Can build index. Can create POM file. Some issues with compilation errors detection.

---
+++ trunk/src/main/java/org/pomizer/model/IndexInfo.java
@@ -0,0 +1,45 @@
+package org.pomizer.model;
+
+public class IndexInfo {
+	
+	public String version;
+	
+	public int basePathsCount;
+	public int jarsCount;
+	public int packagesCount;
+	public int classesCount;
+	
+	public String[] basePaths;
+	public String[] jarNames;
+	public int[] jarNamesBasePathIndex;
+	
+	public String[] packageNames;
+	public int[] packageNamesJarIndex;
+	
+	public String[] classNames;
+	public int[] classNamesJarIndex;
+	public int[] classNamesPackageIndex;
+	
+	public IndexInfo(final String version, final int basePathsCount, final int jarsCount, 
+			final int packagesCount, final int classesCount) {
+		
+		this.version = version;
+
+		this.basePathsCount = basePathsCount;
+		this.jarsCount = jarsCount;
+		this.packagesCount = packagesCount;
+		this.classesCount = classesCount;
+		
+		this.basePaths = new String[basePathsCount];
+		
+		this.jarNames = new String[jarsCount];
+		this.jarNamesBasePathIndex = new int[jarsCount];
+		
+		this.packageNames = new String[packagesCount];
+		this.packageNamesJarIndex = new int[packagesCount];
+		
+		this.classNames = new String[classesCount];
+		this.classNamesJarIndex = new int[classesCount];
+		this.classNamesPackageIndex = new int[classesCount];
+	}
+}
---
+++ trunk/src/main/java/org/pomizer/util/StringUtils.java
@@ -0,0 +1,86 @@
+package org.pomizer.util;
+
+import java.util.regex.Pattern;
+
+public class StringUtils {
+
+	public static String extractFileName(final String filePathName) {
+
+		if (null == filePathName) {
+			return null;
+		}
+
+		int dotPos = filePathName.lastIndexOf('.');
+		int slashPos = filePathName.lastIndexOf('\\');
+		if (slashPos == -1)
+			slashPos = filePathName.lastIndexOf('/');
+
+		if (dotPos > slashPos) {
+			return filePathName.substring(slashPos > 0 ? slashPos + 1 : 0,
+					dotPos);
+		}
+
+		return filePathName.substring(slashPos > 0 ? slashPos + 1 : 0);
+	}
+	
+	public static String changeFileExtension(final String fileName, final String newExtension) {
+		
+		String result;
+		int startOfExtension = fileName.lastIndexOf('.');
+		if (-1 == startOfExtension) {
+			result = fileName + '.' + "newExtension";
+		}
+		else {
+			result = fileName.substring(0, startOfExtension + 1) + newExtension;
+		}
+		return result;
+	}
+	
+	public static boolean areEqual(final String first, final String second) {
+		
+		if (first == second) {
+			return true;
+		}
+		
+		if (null != first) {
+			return first.equals(second);
+		}
+		
+		return false;
+	}
+	
+	public static int hashCode(final String string) {
+		if (null == string) {
+			return 0;
+		}
+		return string.hashCode();
+	}
+	
+	public static String convertPathToObjectName(final String path) {
+		
+		if (null == path) {
+			return "";
+		}
+		
+		String normalizedPath = path.replace('\\', ' ').replace(
+				'/', ' ').replace(':', ' ').replace('-', ' ');
+		normalizedPath = normalizedPath.replace('$', ' ');
+		String[] pathParts = normalizedPath.split(Pattern.quote(" "));
+		String result = "";
+		for (int i = 0; i < pathParts.length; i++) {
+			if (!pathParts[i].isEmpty()) {
+				result += "." + pathParts[i]; 
+			}
+		}
+		
+		if (result.length() > 0) {
+			result = result.substring(1).toLowerCase();
+		}
+		
+		return result;
+	}
+	
+	public static boolean isNullOrEmpty(final String value) {
+		return (null == value) || value.isEmpty();
+	}
+}
