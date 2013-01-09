3b0bbd8677281a42e92c891514b868dcecb71554
AndriyPt@gmail.com <AndriyPt@gmail.com@96db4dfc-c75a-a41e-586d-6ff218abbca7>
2013-01-09 21:36:44 +0000

    Fixed formatting of the code. Updated SVN ignore file list.

--- src/main/java/org/pomizer/constant/CompilationConstants.java
+++ src/main/java/org/pomizer/constant/CompilationConstants.java
@@ -1,32 +1,32 @@
 package org.pomizer.constant;
 
 public class CompilationConstants {
-	
-	public static final String COMPILATION_ERROR_HEADER = "[ERROR] COMPILATION ERROR :";
-	
-	public static final String ERROR_PREFIX = "[ERROR] ";
 
-	public static final String PACKAGE_ERROR_PREFIX = "package ";
-	
-	public static final String PACKAGE_ERROR_SUFIX = " does not exist";
-	
-	public static final String SYMBOL_ERROR_MESSAGE = "cannot find symbol";
-	
-	public static final String SYMBOL_ERROR_PREFIX = "symbol: class ";
-	
-	public static final String SYMBOL_CLASS_ERROR_PREFIX = "symbol  : class ";
-	
-	public static final String SYMBOL_METHOD_ERROR_PREFIX = "symbol  : method ";
-	
-	public static final String SYMBOL_LOCATION_INTERFACE_PREFIX = "location: interface ";
-	
-	public static final String CLASS_LOCATION_PACKAGE_PREFIX = "location: package ";
-	
-	public static final String CANNOT_ACCESS_ERROR = "cannot access ";
-	
-	public static final String CANNOT_ACCESS_ERROR_PREFIX = "class file for ";
+    public static final String COMPILATION_ERROR_HEADER = "[ERROR] COMPILATION ERROR :";
 
-	public static final String CANNOT_ACCESS_ERROR_SUFIX = " not found";
-	
-	public static final String BUILD_SUCCESSFUL = "[INFO] BUILD SUCCESSFUL";
+    public static final String ERROR_PREFIX = "[ERROR] ";
+
+    public static final String PACKAGE_ERROR_PREFIX = "package ";
+
+    public static final String PACKAGE_ERROR_SUFIX = " does not exist";
+
+    public static final String SYMBOL_ERROR_MESSAGE = "cannot find symbol";
+
+    public static final String SYMBOL_ERROR_PREFIX = "symbol: class ";
+
+    public static final String SYMBOL_CLASS_ERROR_PREFIX = "symbol  : class ";
+
+    public static final String SYMBOL_METHOD_ERROR_PREFIX = "symbol  : method ";
+
+    public static final String SYMBOL_LOCATION_INTERFACE_PREFIX = "location: interface ";
+
+    public static final String CLASS_LOCATION_PACKAGE_PREFIX = "location: package ";
+
+    public static final String CANNOT_ACCESS_ERROR = "cannot access ";
+
+    public static final String CANNOT_ACCESS_ERROR_PREFIX = "class file for ";
+
+    public static final String CANNOT_ACCESS_ERROR_SUFIX = " not found";
+
+    public static final String BUILD_SUCCESSFUL = "[INFO] BUILD SUCCESSFUL";
 }
--- src/main/java/org/pomizer/util/XmlUtils.java
+++ src/main/java/org/pomizer/util/XmlUtils.java
@@ -17,103 +17,98 @@ import org.jaxen.dom4j.Dom4jXPath;
 import org.pomizer.constant.XmlConstants;
 import org.pomizer.model.Dependency;
 
-
 public class XmlUtils {
-	
-	public static String getChildNodeTrimmedText(final Node parent, final String childName) {
-		return getChildNodeTrimmedText(parent, childName, false);
-	}
-	
 
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public static String getChildNodeTrimmedText(final Node parent, final String childName, 
-			final boolean usePomNamespace) {
+    public static String getChildNodeTrimmedText(final Node parent, final String childName) {
+        return getChildNodeTrimmedText(parent, childName, false);
+    }
 
-		String result = "";
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public static String getChildNodeTrimmedText(final Node parent, final String childName,
+            final boolean usePomNamespace) {
 
-		if (null != parent) {
+        String result = "";
 
-			try {
-				HashMap map = new HashMap();
-				map.put("default", XmlConstants.POM_DEFAULT_NAMESPACE);
-				
-				String prefix = "./";
-				if (usePomNamespace) {
-					prefix += "default:";
-				}
-				XPath xpath = new Dom4jXPath(prefix + childName);
-				xpath.setNamespaceContext(new SimpleNamespaceContext(map));
-		
-			    Node child = (Node) xpath.selectSingleNode(parent);
-				if (null != child) {
-					if (null != child.getText()) {
-						result = child.getText().trim();
-					}
-				}
-			} 
-			catch (JaxenException e) {
-				e.printStackTrace();
-			}
-		}
+        if (null != parent) {
 
-		return result;
-	}
+            try {
+                HashMap map = new HashMap();
+                map.put("default", XmlConstants.POM_DEFAULT_NAMESPACE);
 
-	public static Document downloadMavenXml(final String urlToGo) throws DocumentException {
+                String prefix = "./";
+                if (usePomNamespace) {
+                    prefix += "default:";
+                }
+                XPath xpath = new Dom4jXPath(prefix + childName);
+                xpath.setNamespaceContext(new SimpleNamespaceContext(map));
 
-		Document document = null;
-		try {
-			URL url = new URL(urlToGo);
-			SAXReader reader = new SAXReader();
-			document = reader.read(url);
-		} 
-		catch (MalformedURLException e) {
-			e.printStackTrace();
-		}
-		return document;
-	}
-	
-	public static Dependency readDependencyFromXml(final Node node) {
-		return readDependencyFromXml(node, false);
-	}
-	
-	public static Dependency readDependencyFromXml(final Node node, final boolean usePomNamespace) {
-		Dependency result = new Dependency();
-		if (null != node) {
-			result.groupId = getChildNodeTrimmedText(
-					node, XmlConstants.ARTIFACT_GROUP_ID, usePomNamespace); 
-			result.artifactId = getChildNodeTrimmedText(
-					node, XmlConstants.ARTIFACT_ID, usePomNamespace); 
-			result.version = getChildNodeTrimmedText(
-					node, XmlConstants.ARTIFACT_VERSION, usePomNamespace);
-		}
-		return result;
-	}
-	
-	@SuppressWarnings("rawtypes")
-	public static List<Dependency> searchForAllDependecyArtifacts(final Document document) {
+                Node child = (Node) xpath.selectSingleNode(parent);
+                if (null != child) {
+                    if (null != child.getText()) {
+                        result = child.getText().trim();
+                    }
+                }
+            }
+            catch (JaxenException e) {
+                e.printStackTrace();
+            }
+        }
 
-		List<Dependency> result = new ArrayList<Dependency>();
-		if (null != document) {
-			List nodes = document.selectNodes("/searchNGResponse/data/artifact");
-			for (int i = 0; i < nodes.size(); i++) {
-				if (nodes.get(i) instanceof Node) {
-					Dependency dependency = readDependencyFromXml((Node)nodes.get(i));
-					boolean found = false;
-					for (int j = 0; (j < result.size()) && !found; j++) {
-						Dependency currentDependency = result.get(j); 
-						if (StringUtils.areEqual(currentDependency.groupId, dependency.groupId) &&
-								StringUtils.areEqual(currentDependency.artifactId, dependency.artifactId)) {
-							found = true;
-						}
-					}
-					
-					if (!found) {
-						result.add(dependency);
-					}
-				}
-			}
-		}
-		return result;
-	}
+        return result;
+    }
+
+    public static Document downloadMavenXml(final String urlToGo) throws DocumentException {
+
+        Document document = null;
+        try {
+            URL url = new URL(urlToGo);
+            SAXReader reader = new SAXReader();
+            document = reader.read(url);
+        }
+        catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return document;
+    }
+
+    public static Dependency readDependencyFromXml(final Node node) {
+        return readDependencyFromXml(node, false);
+    }
+
+    public static Dependency readDependencyFromXml(final Node node, final boolean usePomNamespace) {
+        Dependency result = new Dependency();
+        if (null != node) {
+            result.groupId = getChildNodeTrimmedText(node, XmlConstants.ARTIFACT_GROUP_ID, usePomNamespace);
+            result.artifactId = getChildNodeTrimmedText(node, XmlConstants.ARTIFACT_ID, usePomNamespace);
+            result.version = getChildNodeTrimmedText(node, XmlConstants.ARTIFACT_VERSION, usePomNamespace);
+        }
+        return result;
+    }
+
+    @SuppressWarnings("rawtypes")
+    public static List<Dependency> searchForAllDependecyArtifacts(final Document document) {
+
+        List<Dependency> result = new ArrayList<Dependency>();
+        if (null != document) {
+            List nodes = document.selectNodes("/searchNGResponse/data/artifact");
+            for (int i = 0; i < nodes.size(); i++) {
+                if (nodes.get(i) instanceof Node) {
+                    Dependency dependency = readDependencyFromXml((Node) nodes.get(i));
+                    boolean found = false;
+                    for (int j = 0; (j < result.size()) && !found; j++) {
+                        Dependency currentDependency = result.get(j);
+                        if (StringUtils.areEqual(currentDependency.groupId, dependency.groupId)
+                                && StringUtils.areEqual(currentDependency.artifactId, dependency.artifactId)) {
+                            found = true;
+                        }
+                    }
+
+                    if (!found) {
+                        result.add(dependency);
+                    }
+                }
+            }
+        }
+        return result;
+    }
 }
